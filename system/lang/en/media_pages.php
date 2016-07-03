53318bdff1845afa54e310974d00e3929e7c3c6a
ferg <me@ferg.in>
2016-07-03 14:34:31 +0600

    Admin pages view

--- system/lang/en/media_pages.php
+++ system/lang/en/media_pages.php
@@ -1,5 +1,6 @@
 <?php
 $strings = array(
     'create_page' => 'Create page',
+    'pages_not_found' => 'Pages not found',
 );
 ?>
--- system/lang/ru/media_pages.php
+++ system/lang/ru/media_pages.php
@@ -1,5 +1,6 @@
 <?php
 $strings = array(
     'create_page' => 'Добавить страницу',
+    'pages_not_found' => 'Страницы не найдены',
 );
 ?>
